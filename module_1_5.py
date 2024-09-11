1989c222e7829e7567c587a9a8ac7beda923e8b5
dmitrdix <dmitr_dix@mail.ru>
2024-09-11 19:44:00 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 3, 'number',3.5
+print(immutable_var)
+immutable_var[0] = 'food'
+print(immutable_var)
+mutable_list = [1,3,5,7]
+print(mutable_list)
+mutable_list [0] = 'food'
+print(mutable_list)
