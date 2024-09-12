f48b5bb0020e8f07acb0579c0e4da96c02d5cb21
dmitrdix <dmitr_dix@mail.ru>
2024-09-12 15:32:49 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict={'Дима':1979, 'Алиса' :2007, 'Марина':1983}
+print(my_dict)
+print(my_dict['Дима'])
+print (my_dict.get('Вася'))
+my_dict.update({'Алескандр':1954, 'Галина':1955})
+my_dict.pop('Алиса')
+print (my_dict.get('Алиса'))
+print(my_dict)
+my_set = {1,3,4,5,1,4,'line','get'}
+print(my_set)
+my_set.update({8,'horror'})
+my_set.remove (5)
+print(my_set)
