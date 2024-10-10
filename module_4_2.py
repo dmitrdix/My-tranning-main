df59f49cdfb414356f5e8c55d9f673a9b4e107f5
dmitrdix <dmitr_dix@mail.ru>
2024-10-10 21:42:08 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -2,4 +2,4 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
     inner_function()        # ничего не происходит
-inner_function()     #  выдает ошибку
+inner_function()     #  выдает ошибку NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
