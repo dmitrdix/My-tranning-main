8db49c611ca62b34d18c35a7ced0e05ed3d75e8d
dmitrdix <dmitr_dix@mail.ru>
2024-12-08 19:30:17 +0300

    Update module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -2,7 +2,9 @@ import requests
 import numpy as np
 import matplotlib.pyplot as plt
 
-#requests
+#requests 
+#Это модуль для языка Python, который используют для упрощения работы с HTTP-запросами..
+#Она предоставляет разработчику обширный пул функций для работы со всеми видами HTTP-запросов.
 url="https://jsonplaceholder.typicode.com/posts/1"
 response = requests.get(url)
 print(response.json())
@@ -10,6 +12,7 @@ print(response.status_code)
 print(response.headers)
 
 #numpy
+#Это библиотека для работы с многомерными массивами.
 a=np.array([[1,2,3],[4,5,6]])
 b=np.array([[2,2,5],[6,8,6]])
 print(a)
@@ -18,6 +21,8 @@ print(a+2)
 print(a+b)
 
 #matplotlib
+#Это библиотека для визуализации данных в Python. Она используется для создания различных видов графиков.
+
 x=[1,2,3,4,5]
 y=[20,34,15,26,58]
 plt.plot(x,y)
