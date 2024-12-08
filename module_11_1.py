2d3f366cb64cfb4f81bf0a93cb541877d4b5e812
dmitrdix <dmitr_dix@mail.ru>
2024-12-08 16:49:01 +0300

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,30 @@
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+
+#requests
+url="https://jsonplaceholder.typicode.com/posts/1"
+response = requests.get(url)
+print(response.json())
+print(response.status_code)
+print(response.headers)
+
+#numpy
+a=np.array([[1,2,3],[4,5,6]])
+b=np.array([[2,2,5],[6,8,6]])
+print(a)
+print(a.ndim)
+print(a+2)
+print(a+b)
+
+#matplotlib
+x=[1,2,3,4,5]
+y=[20,34,15,26,58]
+plt.plot(x,y)
+plt.xlabel('ось x')
+plt.ylabel('ось y')
+plt.plot(x,y,color='blue',marker='o',markersize=9)
+plt.title(' график')
+plt.show()
+plt.scatter(x,y)
+plt.show()
